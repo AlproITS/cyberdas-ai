1c988e0961519b29f811bf2734608a6669221b91
MiraiderHZ <hilzhardi.23@gmail.com>
2023-09-29 07:27:52 +0700

    removed: cleaning up main.py

--- main.py
+++ main.py
@@ -50,42 +50,6 @@ async def demo_get_path_id(path_id: int):
 async def predict(path_id: int):
      return {"message":  f"This is /predict/{path_id} endpoint, use post request to retrieve result"}
 
-taxonomies = [["Normal", "EBO non dysplasique", "EBO en dysplasie de bas grade", "EBO en dysplasie de haut grade", "adénocarcinome superficiel", "adénocarcinome invasif", "Dysplasie de haut grade sur muqueuse épidermoïde", "Carcinome épidermoïde superficiel", "Carcinome épidermoïde invasif"],
-              ["Normal", "Adénome", "Ampullome bénin", "ampullome dégénéré superficiel", "ampullome dégénéré invasif"],
-              ["Normal", "polype glandulokystique", "atrophie", "métaplasie intestinale", "dysplasie de bas grade", "dysplasie de haut grade", "adénocarcinome superficiel", "adénocarcinome invasif", "pancréas aberrant", "GIST"],
-              ["Normal", "Polype hyperplasique", "Adénome festonné sessile", "adénome en dysplasie de bas grade", "adénome en dysplasie de haut grade", "adénocarcinome superficiel", "adénocarcinome invasif"]]
-
-anatomyName = ["Oesophage","Duodénum","Estomac" , "Colon rectum"]
-
-relation_taxo = [[[1,4,4,4,4,4,4,4,4],[4,1,2,4,4,4,4,4,4], [4,2,1,3,4,4,4,4,4], [4,4,3,1,3,4,4,4,4], [4,4,4,3,1,3,4,4,4], [4,4,4,4,3,1,4,4,4], [4,4,4,4,4,4,1,2,3], [4,4,4,4,4,4,2,1,4], [4,4,4,4,4,4,3,4,1]],
-                  [[1,4,4,4,4], [4,1,4,4,4], [4,4,1,3,4], [4,4,3,1,3], [4,4,4,3,1]],
-                  [[1,4,4,4,4,4,4,4,4,4], [4,1,4,4,4,4,4,4,4,4], [4,4,1,2,2,4,4,4,4,4], [4,4,2,1,2,3,4,4,4,4], [4,4,2,2,1,2,4,4,4,4], [4,4,4,3,2,1,3,4,4,4], [4,4,4,4,4,3,1,3,4,4], [4,4,4,4,4,4,3,1,4,4], [4,4,4,4,4,4,4,4,1,4], [4,4,4,4,4,4,4,4,4,1]],
-                  [[1,2,4,4,4,4,4], [2,1,2,4,4,4,4], [4,2,1,4,2,4,4], [4,4,4,1,1,4,4], [4,4,2,1,1,2,4], [4,4,4,4,2,1,3], [4,4,4,4,4,3,1]]]  
-
-feedback = ["", "Les taxonomies peuvent être confondus", "Erreur de conformité (les taxonomies ont une texture et une forme visuelle proche)", "Erreur grave (l'erreur entre deux taxonomies peut causer un risque pour le patient)", "Inacceptable"]
-
-@app.get("/taxonomy/{anatomy}&{prediction}&{anapath}")
-async def giveFeedback(anatomy: str, prediction: str, anapath: str):
-    #Find index
-    anatomy_idx = anatomyName.index(anatomy)
-    prediction_idx = taxonomies[anatomy_idx].index(prediction)
-    anapath_idx = taxonomies[anatomy_idx].index(anapath)
-
-    #Result
-    result = feedback[relation_taxo[anatomy_idx][prediction_idx][anapath_idx]]
-    jsonStr = json.dumps(result, ensure_ascii=False)
-    return {jsonStr}    
-
-@app.get("/taxonomy/{anatomy}")
-async def taxonomyList(anatomy: str):
-    jsonStr = json.dumps(taxonomies[anatomyName.index(anatomy)], ensure_ascii=False)
-    return {jsonStr}
-
-@app.get("/anatomyList")
-async def anatomyList():
-    jsonStr = json.dumps(anatomyName, ensure_ascii=False)
-    return {jsonStr}
-
 @app.post("/predict")
 async def predict(request: Request, requess: Req = Depends(form_req)):
     '''
@@ -106,16 +70,7 @@ async def predict(request: Request, requess: Req = Depends(form_req)):
     data.extend([int(children)])
     data.extend([int(smoker)])
     data.extend([int(region)])  
-    # if sex == 'Male':
-    #     data.extend([0, 1])
-    # else:
-    #     data.extend([1, 0])
 
-    # if smoker == 'Yes':
-    #     data.extend([0, 1])
-    # else:
-    #     data.extend([1, 0])
-    
     prediction = model.predict([data])
     output = round(prediction[0], 2)
 
@@ -130,5 +85,3 @@ async def predict(request: Request, requess: Req = Depends(form_req)):
             "smoker": "Ya" if requess.smoker == 1 else "Tidak"
         }
     )
-    
-#     #return render_template('index.html', insurance_cost=output, age=age, sex=sex, smoker=smoker)
